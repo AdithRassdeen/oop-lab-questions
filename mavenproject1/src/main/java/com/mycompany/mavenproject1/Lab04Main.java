185391e91c6d0a04337e06dcf4e702d302c71f98
AdithRassdeen <rassdeenadith@gmail.com>
2026-04-15 23:56:11 +0530

    lab04 q5 static members added

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Lab04Main.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Lab04Main.java
@@ -2,11 +2,14 @@ package com.mycompany.mavenproject1;
 
 public class Lab04Main {
     public static void main(String[] args) {
+        StaffMember.showSystemName();
+        System.out.println("------------------------------------");
         
         Lecturer lec1 = new Lecturer("Dr. Perera", "L001", "Computing", 3, 50000.0);
+        Lecturer lec2 = new Lecturer("Prof. Smith", "L002", "Business", 2, 60000.0);
         LabAssistant lab1 = new LabAssistant("Mr. Silva", "A505", "Engineering", 40, 1500.0);
 
-        System.out.println("--- Lecturer Details ---");
+        System.out.println("\n--- Lecturer 1 Details ---");
         lec1.displayLecturerDetails();
         System.out.println("Monthly Payment: " + lec1.calculateMonthlyPayment());
         
@@ -17,6 +20,9 @@ public class Lab04Main {
         System.out.println("\n--- University Policy Information ---");
         UniversityPolicy.showPolicyHeader();
         System.out.println("University Name: " + UniversityPolicy.UNIVERSITY_NAME);
+        
+        System.out.println("\n------------------------------------");
+        System.out.println("Total Staff Members Created: " + StaffMember.getStaffCount());
         
         double lecBonus = UniversityPolicy.calculateBonus(lec1.calculateMonthlyPayment());
         double labBonus = UniversityPolicy.calculateBonus(lab1.calculateMonthlyPayment());
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/StaffMember.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/StaffMember.java
@@ -3,7 +3,11 @@ package com.mycompany.mavenproject1;
 public abstract class StaffMember {
     /* StaffMember is abstract to prevent direct instantiation of objects,
     as it serves only as a common design for specific staff types.*/
-        
+    private static int staffCount = 0;  
+    /*The staffCount variable is static because it
+     belongs to the class itself rather than any specific object,
+     allowing it to maintain a shared value that tracks the
+     total number of staff members created across the entire system*/
     private String fullName; 
     private final String staffid; 
     protected String department; 
@@ -12,6 +16,7 @@ public abstract class StaffMember {
         this.fullName = fullName;
         this.staffid = staffid;
         this.department = department;
+        staffCount++ ;
     }
    
     public String getFullName() {
@@ -33,5 +38,13 @@ public abstract class StaffMember {
     }
 
     public abstract double calculateMonthlyPayment();
+    
+    public static void showSystemName() {
+        System.out.println("System Name: Campus Staff Payment System");
+    }
+    
+    public static int getStaffCount() {
+        return staffCount;
+    }
 }
 
