ef3753364669b2cf9061e195cc24591bc722a5cc
AdithRassdeen <rassdeenadith@gmail.com>
2026-04-15 23:21:09 +0530

    lab04 q3 second child class

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Lab04Main.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Lab04Main.java
@@ -4,10 +4,14 @@ public class Lab04Main {
     public static void main(String[] args) {
         
         Lecturer lec1 = new Lecturer("Dr. Perera", "L001", "Computing", 3, 50000.0);
+        LabAssistant lab1 = new LabAssistant("Mr. Silva", "A505", "Engineering", 40, 1500.0);
 
         System.out.println("--- Lecturer Details ---");
         lec1.displayLecturerDetails();
-
         System.out.println("Monthly Payment: " + lec1.calculateMonthlyPayment());
+        
+        System.out.println("\n--- Lab Assistant Details ---");
+        lab1.displayLabAssistantDetails();
+        System.out.println("Monthly Payment: " + lab1.calculateMonthlyPayment());
     }
 }
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/LabAssistant.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/LabAssistant.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.mavenproject1;
+package com.mycompany.mavenproject1; 
 
-/**
- *
- * @author LENOVO
- */
-public class LabAssistant {
+public class LabAssistant extends StaffMember {
     
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public LabAssistant(String fullName, String staffid, String department, int hoursWorked, double hourlyRate) {
+        super(fullName, staffid, department);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double calculateMonthlyPayment() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayLabAssistantDetails() {
+        displayBasicDetails();
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Hourly Rate: " + hourlyRate);
+        /* The 'department' variable can be used directly in child classes
+        because it is declared as 'protected' in the StaffMember parent class,
+        which allows access within subclasses.*/
+        
+        System.out.println("Department (Accessed via protected): " + department);
+    }
 }
