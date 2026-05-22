59f2ed00f58edb502f992315784cfd43a663e96f
AdithRassdeen <rassdeenadith@gmail.com>
2026-05-22 23:31:10 +0530

    Appointment

---
+++ mavenproject1/src/test/java/Question3/Appointment.java
@@ -0,0 +1,47 @@
+
+package Lab6;
+public class Appointment {
+    private String patientName;
+    private String appointmentType;
+    private String patientCategory;
+    private double baseFee;
+
+    // Constructor
+    public Appointment(String patientName, String appointmentType, String patientCategory, double baseFee) {
+        this.patientName = patientName;
+        this.appointmentType = appointmentType;
+        this.patientCategory = patientCategory;
+        this.baseFee = baseFee;
+    }
+
+    // Getters
+    public String getPatientName() { return patientName; }
+    public String getAppointmentType() { return appointmentType; }
+    public String getPatientCategory() { return patientCategory; }
+    public double getBaseFee() { return baseFee; }
+
+    // Final Fee eka calculate karana method eka
+    public double calculateFinalFee() {
+        double finalFee = baseFee;
+
+        if (appointmentType.equals("Specialist")) {
+            finalFee = finalFee + 1500;
+        } else if (appointmentType.equals("Online")) {
+            finalFee = finalFee - 500;
+        } 
+
+  
+        if (patientCategory.equals("Child")) {
+            finalFee = finalFee - 300;
+        } else if (patientCategory.equals("Senior")) {
+            finalFee = finalFee - 500;
+        } 
+        
+        return finalFee;
+    }
+
+    public String getAppointmentMessage() {
+        return appointmentType + " appointment selected";
+    }
+}
+
